b0a77431becbc3eaa9212c80e5318604f6dcd1d0
Juan Pulido <juancamilopulido@outlook.com>
2026-04-23 09:02:58 -0400

    Allow deployed Vercel frontend in CORS

--- app/main.py
+++ app/main.py
@@ -11,7 +11,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://abgs-wine.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
